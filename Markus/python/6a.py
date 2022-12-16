277b724494964226b8f5926cb3ac86ab3d1e5f81
mthr4ndir <62054280+mthr4ndir@users.noreply.github.com>
2022-12-16 13:24:39 +0100

    Add day 6-1, inputs 5 and 6
    Add Python solution for Day 6 Part 1, inputs of days 5 and 6

---
+++ Markus/python/6a.py
@@ -0,0 +1,19 @@
+import fileinput
+
+x=0
+y=4
+sub=[]
+for line in fileinput.input():
+    line=line.strip()
+
+    sig=[]
+    for c in line:
+        sig.append(c)
+    
+    while y<len(sig)+1:
+        sub=sig[x:y]
+        if len(sub)==len(set(sub)):
+            print(y)
+            break
+        x+=1
+        y+=1
